cb09361c13b2d6592c32613fbbb8cc805bd0f65c
Mahmoud Shalaby <mhmooud074@gmail.com>
2022-09-13 20:06:27 +0200

    create commands.txt

--- setup.bat
+++
@@ -1,9 +0,0 @@
-pip install django
-cd frontend
-npm i
-start cmd /k "npm run dev -- --open --host"
-echo hi
-start cmd.exe /c echo hi
-cd ..
-start cmd /k "py manage.py runserver 0.0.0.0:8000"
-pause
